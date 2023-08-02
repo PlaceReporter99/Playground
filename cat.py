5f1648736554b2ee02f85d8e03add7754b6ef248
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-02 16:00:50 +0100

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,1 +1,7 @@
-print(input("Enter input:"))
+def input(prompt, file):
+  print(prompt, end="")
+  with open(file) as f:
+    print(inn := f.readline())
+  return inn
+print(input("Enter input: "))
+print(input("Enter input: "))
